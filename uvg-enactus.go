667688fcafd825c8277da8c513d1e74f35f40499
Diego Sosa <stdjca@gmail.com>
2016-12-05 00:25:54 -0600

    change to gin

--- uvg-enactus.go
+++ uvg-enactus.go
@@ -4,25 +4,15 @@ import (
   "net/http"
   "log"
   "os"
-  "github.com/gorilla/mux"
+  "github.com/gin-gonic/gin"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Gorilla!\n"))
-}
-
 func main() {
-  port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Println("$PORT must be set. Running localhost:8000.")
-    port = ":8000"
-	}
-
-  r := mux.NewRouter()
-  // Routes consist of a path and a handler function.
-  r.HandleFunc("/", YourHandler)
-
-  // Bind to a port and pass our router in
-  log.Fatal(http.ListenAndServe(":8000", r))
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H {
+      "message": "pong",
+    })
+  })
+  r.Run()
 }
